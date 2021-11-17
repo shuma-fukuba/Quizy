deba045903eca997f805a189aa96b30b42db7263
fukuba-shuma <calculus.fukuba@gmail.com>
2021-11-17 16:00:11 +0900

    クイズ正解、不正解機能実装

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = {"0-1-0": false, "0-1-1": false, "0-1-2": true};
+
+function checkAnswer(a, b, c) {
+    let question = String(a) + "-" + String(b) + "-" + String(c);
+    let element = document.getElementById(question);
+    const result_id = "a-" + String(b);
+    let quiz_result = document.getElementById(result_id);
+    quiz_result.style.display = "block";
+    if (questions[question]) {
+        // cssに正解クラスを追加
+        element.classList.add("correct");
+        let successed = quiz_result.getElementsByClassName("quiz-result-title");
+        successed.classList.add("quiz-result-successed");
+    } else {
+        // cssに不正解クラスを追加
+        element.classList.add("incorrect");
+        let failed = quiz_result.getElementsByClassName("quiz-result-title")
+        failed.classList.add("quiz-result-failed");
+    }
+};
