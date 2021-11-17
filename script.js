5778c08c29ba9af0014f3cfb84d730a418581533
fukuba-shuma <calculus.fukuba@gmail.com>
2021-11-17 18:59:59 +0900

    ループ実装1(正解判定機能要修正)

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let q_num = 0;
+let q_num = 0; // 問題番号
 
-var Question = function (name, correct, incre1, incre2, img) {
+function Question(name, correct, incre1, incre2, img) {
     // __init__
     q_num++;
     this.name = name;
     this.correct = correct;
-    this.incre1 = incre1;
-    this.incre2 = incre2;
-
+    this.num = q_num;
     this.getImgUrls = function(){
         return "./img/quizes/" + img;
     }
@@ -16,19 +14,36 @@ var Question = function (name, correct, incre1, incre2, img) {
         cor_num = "0-" + String(q_num) + "-2";
         incr1_num = "0-" + String(q_num) + "-1";
         incre2_num = "0-" + String(q_num) + "-0";
-        return {cor_unm: true, incre1: false, incre2: false};
-    }   
-
+        const cor_choice = new Choice(cor_num, correct, true);
+        const inc_choice1 = new Choice(incr1_num, incre1, false);
+        const inc_choice2 = new Choice(incre2, incre2, false);
+        return [cor_choice, inc_choice1, inc_choice2];
+    }
 };
 
-// questions はテスト用
-const questions = { "0-1-0": false, "0-1-1": false, "0-1-2": true };
-q1 = Question("高輪", "たかなわ", "こうわ", "たかわ", 1, "takanawa.png");
-q2 = Question("亀戸", "かめいど", "かめど", "かめと", 2, "takanawa.png");
+function Choice(num, sel, isCorrect){
+    this.num = num; //str
+    this.sel = sel; //str
+    this.isCorrect = isCorrect; //bool
+}
 
-function checkAnswer(a, b, c) {
-    let question = String(a) + "-" + String(b) + "-" + String(c);
-    let element = document.getElementById(question);
+// questions はテスト用
+// const questions = { "0-1-0": false, "0-1-1": false, "0-1-2": true };
+const takanawa = new Question("高輪", "たかなわ", "こうわ", "たかわ", "takanawa.png");
+const kameido = new Question("亀戸", "かめいど", "かめど", "かめと", "kameido.png");
+const koujimachi = new Question("麹町", "こうじまち", "おかとまち", "かゆまち", "koujimachi.png");
+const onarimon = new Question("御成門", "おなりもん", "おかどもん", "ごせいもん", "onarimon.png");
+const todoroki = new Question("等々力", "とどろき", "たたら", "たたりき", "todoroki.png");
+const shakuji = new Question("石神井", "しゃくじい", "いじい", "せきこうい", "shakuji.png");
+const zoushiki = new Question("雑色", "ぞうしき", "ざっしょく", "ざっしき", "zoushiki.png");
+const okachimachi = new Question("御徒町", "おかちまち", "ごしろちょう", "みとちょう", "okachimachi.png");
+const shishibone = new Question("鹿骨", "ししぼね", "しこね", "ろっこつ", "shishibone.png");
+const kogure = new Question("小榑", "こぐれ", "こしゃく", "こばく", "kogure.png");
+
+const questions = [takanawa, kameido, koujimachi, onarimon, todoroki, shakuji, zoushiki, okachimachi, shishibone, kogure];
+
+function checkAnswer(answer) { // Choice型
+    let element = document.getElementById(answer.num);
     const result_id = "a-" + String(b);
     let quiz_result = document.getElementById(result_id);
     quiz_result.style.display = "block";
